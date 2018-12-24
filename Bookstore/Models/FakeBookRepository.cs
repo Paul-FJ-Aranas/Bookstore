e17a50c7a070d5e0408ae1794cf1f89e2a64aebc
Paul Aranas <paul.aranas@ymail.com>
2018-12-24 09:32:28 -0800

    feat: Add a FakeBookRepository to the Models folder

---
+++ Bookstore/Models/FakeBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public class FakeBookRepository : IBookRepository
+    {
+        public IQueryable<Book> Book => new List<Book> {
+            new Book {Title = "The Idiot", Price = 20},
+            new Book {Title = "Don Quioxte", Price = 15},
+            new Book {Title = "Grapes of Wrath", Price = 18}
+        }.AsQueryable<Book>();
+    }
+}
