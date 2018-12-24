ed6fe9edfa4c1db712c45f0aaab98e946251e37c
Paul Aranas <paul.aranas@ymail.com>
2018-12-24 10:16:18 -0800

    feat: Add code to register the repository service

--- Bookstore/Startup.cs
+++ Bookstore/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Bookstore.Models;
 
 namespace Bookstore
 {
@@ -18,6 +19,7 @@ namespace Bookstore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IBookRepository, FakeBookRepository>();
             services.AddMvc();
         }
 
