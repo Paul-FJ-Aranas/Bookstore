6989545a7e282440f06744496157dc5b926c5088
Paul Aranas <paul.aranas@ymail.com>
2018-12-26 13:33:34 -0800

    feat: Set the default route in the Startup class

--- Bookstore/Startup.cs
+++ Bookstore/Startup.cs
@@ -42,7 +42,7 @@ namespace Bookstore
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=List}/{id?}");
             });
         }
     }
