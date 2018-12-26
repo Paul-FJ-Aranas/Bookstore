0fe931a913d0608b7bf66cd7ebfd24c1ace2b598
Paul Aranas <paul.aranas@ymail.com>
2018-12-25 16:06:09 -0800

    feat: Add List view to Views folder, fix syntax by adding two semicolons to properties in Book.cs

--- Bookstore/Models/Book.cs
+++ Bookstore/Models/Book.cs
@@ -11,8 +11,8 @@ namespace Bookstore.Models
         public string Title { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
-        public decimal Price { get; set }
-        public string Category { get; set }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
     }
 }
 
