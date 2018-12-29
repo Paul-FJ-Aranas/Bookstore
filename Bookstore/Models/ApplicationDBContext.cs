8f26efacf3fc85495c2648cd622ef6713686bfce
Paul Aranas <paul.aranas@ymail.com>
2018-12-28 20:59:31 -0800

    feat: Add a database context class to the Models folder.

---
+++ Bookstore/Models/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Bookstore.Models
+{
+    public class ApplicationDBContext : DbContext 
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
