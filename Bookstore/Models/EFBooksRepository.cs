8369c3c7187cc6fe3b68bbed5c5f44a31376bfd7
Paul Aranas <paul.aranas@ymail.com>
2019-01-02 21:37:06 -0800

    feat: Add an EFBookstoreRepository class, add the connection string to appsetting.json

---
+++ Bookstore/Models/EFBooksRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public class EFBooksRepository : IBookRepository
+    {
+        private ApplicationDBContext context;
+
+        public EFBooksRepository(ApplicationDBContext ctx)
+        {
+            context = ctx;
+        }
+
+        IQueryable<Book> IBookRepository.Book => context.Books;
+    }
+}
